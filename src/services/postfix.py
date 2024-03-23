36113eaea089d677487a488276a8d40ee3610467
Niko Raulas <niko.raulas@outlook.com>
2024-03-23 21:35:34 +0200

    postfix konvertterin tekeminen

--- src/services/postfix.py
+++ src/services/postfix.py
@@ -1,7 +1,40 @@
-from collections import deque
-
-
 class Postfixconverter:
     def __init__(self):
-        self.stack = []
-        self.queue = deque([])
+        self.precedence = {"+":1,
+                           "-":1,
+                           "*":2,
+                           "/":2,
+                           "^":3}
+
+    def convert(self, infix):
+        stack = []
+        postfix = []
+
+        for token in infix:
+            if type(token) == int or type(token) == float:
+                postfix.append(token)
+
+            elif token == "(":
+                stack.append(token)
+
+            elif token == ")":
+                while len(stack) > 0:
+                    operator = stack.pop()
+                    if operator == "(":
+                        break
+                    postfix.append(operator)
+
+            else:
+                while (
+                    len(stack) > 0 and stack[-1] != "(" and
+                    (self.precedence[stack[-1]] > self.precedence[token] or 
+                     self.precedence[stack[-1]] == self.precedence[token] and token != "^")
+                     ):
+                    postfix.append(stack.pop())
+
+                stack.append(token)
+
+        while len(stack) > 0:
+            postfix.append(stack.pop())
+
+        return postfix
