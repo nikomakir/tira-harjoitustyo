dc929cd7957e07d9dc5116c1ce28730a8e192fba
Niko Raulas <niko.raulas@outlook.com>
2024-03-23 22:37:03 +0200

    evaluaattori luokan tekeminen

---
+++ src/services/evaluator.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+
+class Evaluator:
+    """Luokka, joka laskee matemaattisen lausekkeen postfix notaatiomuodossa ja palauttaa vastauksen
+    """
+
+    def evaluate(self, postfix):
+        """Metodi, joka laskee potfix muotoisen lausekkeen arvon.
+
+        Argumentit:
+            postfix (lista): Ottaa listana postfix muodossa olevan lausekkeen. Muutta tämän deque -jonoksi.
+
+        Returns:
+            int/float: Palauttaa lausekkeen arvon.
+        """
+
+        stack = []
+        queue = deque(postfix)
+
+        while len(queue) > 0:
+            token = queue.popleft()
+            if type(token) == int or type(token) == float:
+                stack.append(token)
+            else:
+                second_operand = stack.pop()
+                first_operand = stack.pop()
+                match token:
+                    case "+":
+                        stack.append(first_operand + second_operand)
+                    case "-":
+                        stack.append(first_operand - second_operand)
+                    case "*":
+                        stack.append(first_operand * second_operand)
+                    case "^":
+                        stack.append(first_operand ** second_operand)
+                    case "/":
+                        try:
+                            result = first_operand / second_operand
+                            stack.append(result)
+                        except ZeroDivisionError:
+                            return "Virhe: Nollalla jako"
+  
+        return stack.pop()
+    
